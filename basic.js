d09e1e53a6fc8979d261c70cf836e7b127fd64b1
sagar pratap <123818181+sagarpratap1993@users.noreply.github.com>
2023-07-06 09:49:08 -0700

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,135 @@
+/*var 1a = 10 (we can not write no inside the variable)*/
+/* console.log used as output instruction*/
+
+/*var a = 10
+var b= 15.36
+var c="sagar"
+var d="ducat education Noida"
+var e=`HI
+          my name is sagar `
+
+var f= true
+var g=false          
+
+console.log(a,typeof(a))
+console.log(b,typeof(b))
+console.log(c,typeof(c))
+console.log(d,typeof(d))
+console.log(g,typeof(g))*/
+
+/*var a = 20
+var b = 20
+var sum=a+b
+console.log(sum)
+console.log(a,"+",b,"=",sum)
+console.log(a+"+"+b+"="+sum) /*string concadination*/
+/*console.log(`${a} + ${b} = ${sum}`)*/
+
+/*var p= 535555
+var r= 12.36
+var t= 5
+var si= p*r*t/100
+var total= p+si
+var emi= total/(t*12)
+console.log("principle amount"+p)
+console.log("rate............"+r)
+console.log("time............"+t)
+console.log("simple interest."+si.toFixed(2))
+console.log("total amount...."+total.toFixed(2))
+console.log("emi............."+emi.toFixed(2))*/
+
+
+
+/*var p= 535555
+var r= 12.36
+var t= 5
+var si= p*r*t/100
+var total= p+si
+var emi= total/(t*12)
+console.log(`principle amount=${p}
+            rate............=${r}
+            time............=${t}
+            simple interest.=${si.toFixed(2)}
+            total amount....=${total.toFixed(2)}
+           emi............. =${emi.toFixed(2)}`)*/
+
+
+
+console.log(Math.floor(1.9999))
+console.log(Math.floor(1.3))
+
+console.log(Math.ceil(1.9999))
+console.log(Math.ceil(2.666))
+
+console.log(Math.round(1.9999))
+console.log(Math.round(1.2))
+
+var l= 5
+var b= 6
+var h= 7
+area = 2*(l*b+b*h+h*l)
+console.log("area of cuboid ="+area)
+
+var a=25
+var b=75
+console.log(`before swapping a= ${a} and b=${b}`)
+
+var temp=a
+a=b
+b=temp
+console.log(`after swapping a = ${a} and b=${b}`)
+
+/* swapping without using third variable*/
+
+var a = 25
+var b = 75
+console.log(`before swaping a= ${a} and b=${b}`)
+
+a=a+b
+b=a-b
+a=a-b
+
+console.log(`after swaping a= ${a} and b=${b}`)
+
+//input instruction
+
+//console.warn("hello world")
+//document.write("<p>hello world<p>")
+//document.write("<p>sagar rahul<P>")
+//console.error("hello world!!!")
+//document.write("<p>hello<p>")
+//document.write("<h1>hello world!!<h1>")
+//document.write("<h1>sagar<h1>")
+//document.write("<p1>I am sagar pratap<p1>")
+
+//var a= Number(prompt("enter a new number :"))
+//document.write(`a= ${a} and type of a = ${typeof (a)}`)
+
+/*var p= Number(prompt("enter a principle no :"))
+var r= Number(prompt("enter a total rate :"))
+var t= Number(prompt("enter a total time in years :"))
+var si= p*r*t/100
+var total= p+si
+var emi= total/(t*12)
+document.write(`principle amount=${p}<br>
+            rate............=${r}<br>
+            time............=${t}<br>
+            simple interest.=${si.toFixed(2)}<br>
+            total amount....=${total.toFixed(2)}<br>
+           emi............. =${emi.toFixed(2)}`)*/
+
+
+
+/*var l = (Number(prompt("enter a lenth :")))
+var b =(Number(prompt("enter a breath :")))
+var h = (Number(prompt("enter a height: ")))
+var area = 2*(l*b+b*h+h*l)
+document.write(`area of cuboid is ${area}`) */ 
+console.log("i am sagar pratap ")  
+
+
+
+       
+
+
+
